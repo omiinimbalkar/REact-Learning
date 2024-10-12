72858ec86fb2841bca9d457663606466171e4500
omiinimbalkar <omn2068@gmail.com>
2024-10-13 00:23:29 +0530

    day2

--- Fragments18/learningFragements/src/App.jsx
+++ Fragments18/learningFragements/src/App.jsx
@@ -2,17 +2,36 @@ import React from 'react'
 import './App.css'
 
 function App() {
+  let ListItem = ['Apples,Bananas', 'Sweet potatoes,Cucumbers', 'Brownrice,Oats', 'Milk,Juice', 'Olive oil,Coconut oil']
+  // return (
+    // <>
+    // <h1>Healthy Food</h1>
+    // {/* after creating array delete other list items and live one item list only */}
+    // <ul className="list-group">
+    //   {ListItem.map(item => <li key={item} className="list-group-item">{item}</li>)}
+    // {/* maps is easy to one line code and run it  */}
+    // {/* loop is not written in jsx */}
+    // {/* <li class="list-group-item"></li>
+    // <li class="list-group-item"></li>
+    // <li class="list-group-item"></li>
+    // <li class="list-group-item"></li> */}
+    // </ul>
+    // </>
 
-  return <React.Fragment>
- <h1>Healthy Food</h1>
-  <ul class="list-group">
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-</ul>
-  </React.Fragment>
+    // second 20.conditional Rendering 
+    if (ListItem.length === 0){
+      return <h3>I am still Empty smotach </h3>
+    }
+    // this if condition run till if the aaray is full then inside if condition not print if array is empty the it print inside if condition
+    return(
+    <>
+      <h1>Healthy Food</h1>
+      <ul className="list-group">
+        {ListItem.map(item => <li key={item} className="list-group-item">{item}</li>)}
+      </ul>
+
+    </>
+  );
 }
 
 export default App
